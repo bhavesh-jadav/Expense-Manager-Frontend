343978d552cbb57a8912f2ea5b87403ff481e32b
bhavesh-jadav <bhaveshhjadav@gmail.com>
2017-09-17 19:26:50 +0530

    modified user dashboard

--- src/app/user/dashboard/dashboard.component.ts
+++ src/app/user/dashboard/dashboard.component.ts
@@ -10,11 +10,22 @@ import { Component, OnInit } from '@angular/core';
 })
 export class DashboardComponent implements OnInit {
 
+  monthNames = [
+    'January', 'February', 'March', 'April',
+    'May', 'June', 'July', 'August', 'September',
+    'October', 'November', 'December'
+  ];
+
   categoryPieChartData: any;
   showCategoryPieChart = false;
   categoryPiChartView: any[] = [600, 300];
   isNewUser = false;
   userName: string;
+  monthWiseBarChartData = [];
+  expenseData = [];
+  dates = [];
+  startDate: string;
+  endDate: string;
 
   constructor(
     private expenseService: ExpenseService,
@@ -28,11 +39,58 @@ export class DashboardComponent implements OnInit {
     this.userName = userData.username;
   }
 
+
+
+  generateMonthWiseBarChartData() {
+
+
+    let dates = [];
+    for (let exp of this.expenseData) {
+      dates.push(exp.date_added.slice(0, 7) + '-01');
+    }
+
+    let uniqueDates = [];
+
+    for (let date of dates) {
+      if (uniqueDates.indexOf(date) < 0) {
+        uniqueDates.push(date);
+      }
+    }
+
+    this.dates = uniqueDates.sort((n1, n2) => {
+      if (n1 > n2) {
+          return 1;
+      } else if (n1 < n2) {
+          return -1;
+      }
+      return 0;
+    });
+
+    for (let date of this.dates) {
+      this.monthWiseBarChartData.push({
+        name: date.slice(0, 7) + '-01',
+        value: 0
+      });
+    }
+    for (let exp of this.expenseData) {
+      for (let data of this.monthWiseBarChartData) {
+        const date_added = exp.date_added.slice(0, 7) + '-01';
+        if (data.name === date_added) {
+          data.value += exp.amount as number;
+        }
+      }
+    }
+
+    this.startDate = this.dates[0];
+    this.startDate = this.dates[6];
+  }
+
   showCategoryPiChart () {
     this.expenseService.getExpenses().subscribe(
       response => {
-        const expenseData = response.json();
-        if (expenseData.length > 0) {
+        this.expenseData = response.json();
+        this.generateMonthWiseBarChartData();
+        if (this.expenseData.length > 0) {
           this.expenseService.getExpenseCategories().subscribe(
             responseCat => {
               const categories = responseCat.json();
@@ -43,7 +101,7 @@ export class DashboardComponent implements OnInit {
                   value: 0
                 });
               }
-              for (let exp of expenseData) {
+              for (let exp of this.expenseData) {
                 for (let d of data) {
                   if (d.name === exp.category) {
                     d.value += exp.amount as number;
